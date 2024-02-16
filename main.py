3a3ec0cd998a56f0b106d6665978fd886a05a08f
victorprens <vprens12@gmail.com>
2024-02-16 13:15:47 -0500

    Hirst Painting second step.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("red")
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
+tim = Turtle()
+tim.shape("turtle")
+tim.color("red")
+
+for _ in range(4): #Create a square
+    tim.forward(100)
+    tim.right(90)
 
 screen = Screen()
 screen.exitonclick()
